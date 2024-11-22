2774591e73e76270c402cc2eb57b4bccf613b50a
Becky Smith <becky.smith@thedatalab.org>
2024-11-22 11:22:22 +0000

    Tweaks to the ehrql section and dataset def files

--- analysis/ehrql_dataset_definition_hospitalised_deaths.py
+++ analysis/ehrql_dataset_definition_hospitalised_deaths.py
@@ -1,4 +1,4 @@
-from ehrql import create_dataset, months, debug
+from ehrql import create_dataset, months
 from ehrql.tables.core import patients, ons_deaths
 from ehrql.tables.tpp import apcs, practice_registrations
 
@@ -6,14 +6,21 @@ from ehrql.tables.tpp import apcs, practice_registrations
 study_start_date = "2022-01-01"
 study_end_date = "2023-01-01"
 
-died_in_study_period = ons_deaths.date.is_on_or_between(study_start_date, study_end_date)
+died_in_study_period = ons_deaths.date.is_on_or_between(
+    study_start_date, study_end_date
+)
 
 hospitalisation_in_6_months_before_death = (
     apcs
-    .where(apcs.admission_date.is_on_or_between((ons_deaths.date - months(6)), ons_deaths.date))
+    .where(
+        apcs.admission_date.is_on_or_between(
+            (ons_deaths.date - months(6)), ons_deaths.date
+        )
+    )
     .sort_by(apcs.admission_date)
-   .last_for_patient()
-).admission_date
+    .last_for_patient()
+)
+hospitalised_date = hospitalisation_in_6_months_before_death.admission_date
 
 # registration info at ons-registered date of death
 registration = practice_registrations.for_patient_on(ons_deaths.date)
@@ -25,11 +32,10 @@ dataset.region = registration.practice_nuts1_region_name
 dataset.cause_of_death = ons_deaths.underlying_cause_of_death
 dataset.date_of_death = ons_deaths.date
 
-dataset.hospitalised_date = hospitalisation_in_6_months_before_death
+dataset.hospitalised_date = hospitalised_date
 
 dataset.define_population(
-    died_in_study_period & 
-    hospitalisation_in_6_months_before_death.is_not_null() & 
-    ons_deaths.underlying_cause_of_death.is_not_null()
+    died_in_study_period
+    & hospitalisation_in_6_months_before_death.is_not_null()
+    & ons_deaths.underlying_cause_of_death.is_not_null()
 )
-debug(dataset)
--- analysis/ehrql_dummy_data_london_adults.py
+++ analysis/ehrql_dummy_data_london_adults.py
@@ -2,6 +2,7 @@ from ehrql import create_dataset
 from ehrql.tables.core import patients
 from ehrql.tables.tpp import addresses
 
-possible_msoas = ["E02000001", "E02000002", "E02000003", "E02000004"]
-
 dataset = create_dataset()
+age = patients.age_on("2024-01-01")
+
+possible_msoas = ["E02000001", "E02000002", "E02000003", "E02000004"]
